c66041f9fa49104f09f10af3d1e42fa706a6ca33
Mauriizio <caballeromaurizio@gmail.com>
2025-12-20 23:24:34 -0300

    Actualizando assest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-
 import "./globals.css"
+
+
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -25,6 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon-180.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body className={`font-sans antialiased`}>
         {children}       
       </body>
